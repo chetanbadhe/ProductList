373979f2694c7a167d90563e5226bcc825fd8c0d
Chetan Badhe <chetanbadhe7@gmail.com>
2020-06-13 13:05:44 -0400

    12 commit

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -6,30 +6,27 @@ import { catchError, tap, map } from 'rxjs/operators';
 import { IProduct } from './product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  private productUrl = 'api/products/products.json';
+  private productUrl = 'assets/products/products.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.productUrl)
-      .pipe(
-        tap(data => console.log('All: ' + JSON.stringify(data))),
-        catchError(this.handleError)
-      );
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap((data) => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
 
   getProduct(id: number): Observable<IProduct | undefined> {
-    return this.getProducts()
-      .pipe(
-        map((products: IProduct[]) => products.find(p => p.productId === id))
-      );
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find((p) => p.productId === id))
+    );
   }
 
   private handleError(err: HttpErrorResponse) {
-
     let errorMessage = '';
     if (err.error instanceof ErrorEvent) {
       errorMessage = `An error occurred: ${err.error.message}`;
